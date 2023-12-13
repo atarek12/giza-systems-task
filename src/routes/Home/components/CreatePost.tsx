13142e72f92dadc1778d20ae9b6a6810410c0430
Ahmed Tarek <a7md.tare2@gmail.com>
2023-12-13 16:23:57 +0200

    validating create-post form

--- src/routes/Home/components/CreatePost.tsx
+++ src/routes/Home/components/CreatePost.tsx
@@ -10,8 +10,16 @@ import React from "react";
 import { FormInput } from "../../../shared/components";
 import { useFetcher } from "react-router-dom";
 import { FormProvider, useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const UserIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const schema = Yup.object({
+  title: Yup.string().required("This field is required!"),
+  body: Yup.string().required("This field is required!"),
+  userId: Yup.number().required("This field is required!"),
+}).required();
 
 export type TCreatePostFormValues = {
   userId: number;
@@ -25,11 +33,16 @@ const CreatePost: React.FC<CreatePostProps> = () => {
   const fetcher = useFetcher();
   const useFormAttributes = useForm<TCreatePostFormValues>({
     defaultValues: { body: "", title: "", userId: 1 },
+    resolver: yupResolver<TCreatePostFormValues>(schema),
   });
+
+  const onSubmit = (values: TCreatePostFormValues) => {
+    fetcher.submit(values, { method: "POST" });
+  };
 
   return (
     <FormProvider {...useFormAttributes}>
-      <fetcher.Form method="POST">
+      <form onSubmit={useFormAttributes?.handleSubmit(onSubmit)}>
         <Card border="1px solid">
           <CardHeader>Write what's in your mind...</CardHeader>
 
@@ -53,7 +66,7 @@ const CreatePost: React.FC<CreatePostProps> = () => {
             </Button>
           </CardFooter>
         </Card>
-      </fetcher.Form>
+      </form>
     </FormProvider>
   );
 };
