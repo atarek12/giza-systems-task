1a082ce84a330c9800a5c94783c8658757aa0050
Ahmed Tarek <a7md.tare2@gmail.com>
2023-12-20 23:55:02 +0200

    remove user route

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import { HomeRoute } from "./routes/Home";
 import { Root } from "./routes/Root";
 import { ErrorPage } from "./routes/Error";
-import { UserRoute } from "./routes/User";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
-    children: [HomeRoute, UserRoute],
+    children: [HomeRoute],
   },
 ]);
