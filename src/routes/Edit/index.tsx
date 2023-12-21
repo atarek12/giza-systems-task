7d42253d8eac6fbcc893b55a86e76873f613c6a3
Ahmed Tarek <a7md.tare2@gmail.com>
2023-12-21 13:59:03 +0200

    lazy loading routes

--- src/routes/Edit/index.tsx
+++ src/routes/Edit/index.tsx
@@ -1,9 +1,6 @@
-import { lazy } from "react";
 import { RouteObject } from "react-router-dom";
-
-const Edit = lazy(() => import("./Edit"));
 
 export const EditRoute: RouteObject = {
   path: ":flightId",
-  element: <Edit />,
+  lazy: () => import("./Edit"),
 };
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,9 +1,6 @@
-import { lazy } from "react";
 import { RouteObject } from "react-router-dom";
-
-const Home = lazy(() => import("./Home"));
 
 export const HomeRoute: RouteObject = {
   index: true,
-  element: <Home />,
+  lazy: () => import("./Home"),
 };
